9002301d7af57b893cd0277a4475abb857a5b1cb
Jed <jed@nas.local>
2019-04-24 21:50:47 -0400

    .

--- .toolchest/develop.sh
+++ .toolchest/develop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+git fetch --all
 git remote add --fetch --tags --mirror=push origin-ssh  `git remote get-url origin | sed "s/https:\/\//git@/" | sed "s/.com\//.com:/"`
 git submodule foreach 'git checkout 11.2-RELEASE'
 git submodule foreach 'git config user.email `whoami`@`hostname`-`uname`'
@@ -11,3 +11,4 @@ git submodule foreach 'git push origin-ssh || true'
 git submodule foreach 'git fetch --all || true'
 git commit -am . || true
 git push origin-ssh || true
+git fetch --all

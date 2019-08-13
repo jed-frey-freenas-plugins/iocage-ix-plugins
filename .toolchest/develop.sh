a920c640eede391af252d08b9f9ad597a69a492e
jed <jed@nas>
2019-08-13 19:54:08 -0400

    .

--- .toolchest/develop.sh
+++ .toolchest/develop.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
-
 git remote add --fetch --tags --mirror=push origin-ssh  `git remote get-url origin | sed "s/https:\/\//git@/" | sed "s/.com\//.com:/"`
-git submodule foreach 'git checkout 11.2-RELEASE'
+git submodule foreach 'git checkout 12.0-RELEASE'
 git submodule foreach 'git config user.email `whoami`@`hostname`-`uname`'
 git submodule foreach 'git config user.name `whoami`'
 git submodule foreach 'git remote add --fetch --tags --mirror=push origin-ssh  `git remote get-url origin | sed "s/https:\/\//git@/" | sed "s/.com\//.com:/"` || true'
--- install_jail.sh
+++ install_jail.sh
@@ -2,11 +2,7 @@
 
 PLUGIN=${1%/}
 
-# Interface
-IFACE=lagg0
 # JSON
 JSON=${PLUGIN}/${PLUGIN}.json
 # IP4_addr
-IP4_ADDR="192.168.1.99/24"
-
-iocage fetch --plugin-file --noverify --accept --name ${JSON} ip4_addr="${IP4_ADDR}"
+iocage fetch --plugin-name ${JSON} --noverify --accept ip4_addr="none"

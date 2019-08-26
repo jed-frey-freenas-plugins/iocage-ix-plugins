ca7054d25c8efdf1c45e5ef30a953dc3c5e0bff9
jed <jed@nas>
2019-08-26 01:27:34 -0400

    updated url

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Install the binary package
-#pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.2.0.1/emby-server-freebsd11_4.2.0.1_amd64.txz
-pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.1.1.0/emby-server-freebsd11_4.1.1.0_amd64.txz
+pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.3.0.5/emby-server-freebsd12_4.3.0.5_amd64.txz
 
 # Enable the service
 sysrc -f /etc/rc.conf emby_server_enable="YES"
